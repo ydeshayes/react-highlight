e49f92322752f502e561799e691b852647890313
Smile <deshayes.yann@gmail.com>
2016-10-17 09:43:57 +0200

    Initial commit demo

--- src/Range.js
+++
@@ -1,8 +0,0 @@
-export default class Range {
-  constructor(start, end, text, data = {}) {
-    this.start = start;
-    this.end = end;
-    this.text = text;
-    this.data = data;
-  }
-}
--- src/index.js
+++
@@ -1,4 +0,0 @@
-import Highlightable from './Highlightable';
-
-export default Highlightable;
-export const Range = require('./Range');
